7f79a081e3f875260e3a5afc785456ae85ab46b8
devjv <jv.vansan@gmail.com>
2025-07-17 21:52:44 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// Animações ao rolar
+const sections = document.querySelectorAll('section');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show-section');
+        }
+    });
+}, {
+    threshold: 0.2
+});
+
+sections.forEach(section => {
+    section.classList.add('hidden-section');
+    observer.observe(section);
+});
+// Efeito de digitação
+const text = "Olá, eu sou o João";
+let i = 0;
+
+function type() {
+    if (i < text.length) {
+        document.getElementById("typed").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(type, 100); // velocidade da digitação
+    }
+}
+type();
